33f924cc06e9bc3df2a640f2ded6183d26090ed8
Claudio Esteban <estebanclfe@gmail.com>
2022-11-04 11:34:07 -0300

    agregando plantilla_MVT.html

--- MVT_Esteban/MVT_Esteban/views.py
+++ MVT_Esteban/MVT_Esteban/views.py
@@ -3,6 +3,16 @@ from datetime import datetime
 from django.template import Template, Context, loader
 
 def familiares(request):
+    archivo = open("./templates/plantilla_MVT.html")
+    plantilla = Template(archivo.read())
+    archivo.close()
+
+    contexto = Context()
+
+    pass
+
+def fecha(request):
+
     hoy = datetime.now()
-    lista = 
+    lista = "db.sqlite3"
     return HttpResponse(f"<h1>Lista de Familiares</h1> Hoy es {hoy}")
